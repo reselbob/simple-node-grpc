4d6a98ef47c3538aebb817bc83e7a53f65b8e8b8
Bob Reselman <reselbob@gmail.com>
2020-03-20 12:39:29 -0700

    Intitial Check-in

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# simple-node-grpc
+# Simple Service
+
+This project is a demonstration gRPC API written in Node.JS.
+
+## Description
+
+The `Simple Service` is a gPRC API that publishes four functions:
+
+* add
+* subtract
+* multiply
+* divide
+
+Each function take the message, `Request` as an argument. `Request` is
+an array of floating point numbers.
+ 
+The following is an example of a `Request Message`:
+
+```json
+{
+  "numbers": [
+    5,6,7,8,9
+  ]
+}
+```
+ 
+Each function will process all the numbers in the array in order.
+
+Each function in the API returns a message named `Response`. The following is an example of a 
+`Response` message:
+
+```json
+{
+  "result": 0.00006613756613756613
+}
+```
+
+## Installation
+
+Install the packages.
+
+`npm install`
+
+The start the server.
+
+`node index.js`
+
+
+## The `.proto` File
+
+```proto
+syntax = "proto3";
+
+package simplegrpc;
+
+option objc_class_prefix = "SIMPLEGRPC";
+
+/* Describes an array of floats to be processed */
+message Request {
+    repeated double numbers = 1;
+}
+
+/* Describes the result of processing the submitted array of floats */
+message Response {
+    double result = 1;
+}
+
+service SimpleService {
+    rpc Add (Request) returns (Response) {
+    }
+
+    rpc Subtract (Request) returns (Response) {
+    }
+
+    rpc Multiply (Request) returns (Response) {
+    }
+
+    rpc Divide (Request) returns (Response) {
+    }
+}
+```
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+let server;
+
+const PROTO_PATH = __dirname + '/simple.proto';
+const PORT = process.env.PORT || 50051;
+
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+const simple_proto = grpc.loadPackageDefinition(packageDefinition).simplegrpc;
+
+async function add(call, callback) {
+    const input = call.request.numbers;
+    const result = input.reduce((a, b) => a + b, 0);
+    callback(null, {result});
+}
+
+async function multiply(call, callback) {
+    const input = call.request.numbers;
+    const result = input.reduce((a, b) => a * b, 1);
+    callback(null, {result});
+}
+
+async function subtract(call, callback) {
+    const input = call.request.numbers;
+    const result = input.reduce((a, b) => a - b, 0);
+    callback(null, {result});
+}
+
+async function divide(call, callback) {
+    const input = call.request.numbers;
+    const result = input.reduce((a, b) => a / b, 1);
+    callback(null, {result});
+}
+
+/**
+ * Starts an RPC server that receives requests for the Simpleservice at the
+ * sample server port
+ */
+async function main()  {
+    const implementations = {};
+    implementations.add = add;
+    implementations.subtract = subtract;
+    implementations.multiply = multiply;
+    implementations.divide = divide;
+
+    server = new grpc.Server();
+    server.addService(simple_proto.SimpleService.service, implementations);
+    server.bind(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure());
+    console.log({message: `Starting gRPC Server on port ${PORT}`, startingTime: new Date()});
+    server.start();
+    console.log({message: `Started gRPC Server on port ${PORT}`, startedTime: new Date()});
+}
+
+return main();
+
+module.exports = {server};
