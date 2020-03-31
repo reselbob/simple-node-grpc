40fef241301a0a771de03a731a768d940906e3b8
Bob Reselman <reselbob@gmail.com>
2020-03-31 10:39:10 -0700

    fixed bad trapping bug

--- client_cli/index.js
+++ client_cli/index.js
@@ -4,7 +4,6 @@ const PROTO_PATH = __dirname + '/proto/simple.proto';
 const {SimpleServiceClient} = require('./client');
 
 
-
 var argv = require('yargs')
     .usage('Usage: $0 -o [string] - d [array] -m [string] -c 100 [num')
     .example('$0 -o add -d [4,5.5,6]', 'Sums up the numbers in the array [4, 5.5, 6]')
@@ -28,11 +27,11 @@ var argv = require('yargs')
 
 const client = new SimpleServiceClient(argv.u, PROTO_PATH);
 
-const mathCallback = (err, response) =>{
+const mathCallback = (err, response) => {
     console.log(JSON.stringify(response.result))
 };
 
-const repeatCallback = (err, response) =>{
+const repeatCallback = (err, response) => {
     console.log(response)
 };
 
@@ -47,67 +46,87 @@ const opError = () => {
     console.error(`ERROR: Missing operation. Please declare one of the following operations: add, subtract, divide, multiply, repeat, ping`);
 };
 
-const validateArray = (data) =>{
+const validateArray = (data) => {
     let arr;
     try {
         arr = JSON.parse(data)
     } catch (e) {
-        if(! e instanceof SyntaxError){throw e}
+        if (!e instanceof SyntaxError) {
+            throw e
+        }
     }
-    if(Array.isArray(arr))return arr;
+    if (Array.isArray(arr)) return arr;
 }
 
-const add = (arg) =>{
+const add = (arg) => {
     const arr = validateArray(arg);
-    if(!Array.isArray(arr)) argMathError('add');return;
+    if (!Array.isArray(arr)) {argMathError('add');return;}
     const numbers = arr;
     client.add(numbers, mathCallback);
 };
 
-const subtract = (arg) =>{
+const subtract = (arg) => {
     const arr = validateArray(arg);
-    if(!Array.isArray(arr)) argMathError('subtract');return;
+    if (!Array.isArray(arr)) {argMathError('subtract');return;}
     const numbers = arr;
     client.subtract(numbers, mathCallback);
 };
 
-const divide = (arg) =>{
+const divide = (arg) => {
     const arr = validateArray(arg);
-    if(!Array.isArray(arr)) argMathError('divide');return;
+    if (!Array.isArray(arr)) {argMathError('divide');return;}
     const numbers = arr;
     client.divide(numbers, mathCallback);
 };
 
-const multiply = (arg) =>{
+const multiply = (arg) => {
     const arr = validateArray(arg);
-    if(!Array.isArray(arr)) argMathError('multiply');return;
+    if (!Array.isArray(arr)){argMathError('multiply');return;}
     const numbers = arr;
     client.multiply(numbers, mathCallback);
 };
 
-const repeat = (message, count) =>{
+const repeat = (message, count) => {
     const arr = [];
-    if(typeof  message !== 'string') arr.push('message');
-    if(typeof  count !== 'number') arr.push('count');
-    if(arr.length > 0 ) argRepeatError(arr);return;
+    if (typeof message !== 'string') arr.push('message');
+    if (typeof count !== 'number') arr.push('count');
+    if (arr.length > 0) {argRepeatError(arr); return;}
     client.repeat({message, count}, repeatCallback);
 };
 
-const ping = (message) =>{
-    const callback = (err, response) =>{
+const ping = (message) => {
+    const callback = (err, response) => {
         console.log(`I am pinging the message: ${response.result}.`)
     }
     client.ping(message, callback)
 };
 
-if(typeof argv.o !== 'string'){opError();return};
+if (typeof argv.o !== 'string') {
+    opError();
+    return
+}
+;
 
-switch(argv.o.toLowerCase()){
-    case('add'): add(argv.d);break;
-    case('subtract'): subtract(argv.d);break;
-    case('multiply'): multiply(argv.d);break;
-    case('divide'): divide(argv.d);break;
-    case('repeat'): repeat(argv.m, argv.c);break;
-    case('ping'): ping(argv.m);break;
-    default: console.log(argv.o + ' is an unknown operation')
+switch (argv.o.toLowerCase()) {
+    case('add'):
+        add(argv.d);
+        break;
+    case('subtract'):
+        subtract(argv.d);
+        break;
+    case('multiply'):
+        multiply(argv.d);
+        break;
+    case('divide'):
+        divide(argv.d);
+        break;
+    case('repeat'): {
+        repeat(argv.m, argv.c);
+        break;
+    }
+    case('ping'):
+        ping(argv.m);
+        break;
+    default:
+        console.log(argv.o + ' is an unknown operation')
 }
