a9b5099967f0473fa4c2a3142e83acaa7aaa3e4f
Bob Reselman <reselbob@gmail.com>
2020-03-31 17:15:53 -0700

    enhanced unit tests

--- server/test/server-tests.js
+++ server/test/server-tests.js
@@ -24,8 +24,12 @@ const simplegrpc = grpc.loadPackageDefinition(packageDefinition).simplegrpc;
 const client = new simplegrpc.SimpleService(SERVER_URL,
     grpc.credentials.createInsecure());
 
+const randomIntFromInterval = (min,max) => {
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
 
 describe('Basic gRPC Tests: ', () => {
+
 
     after(function() {
         server.forceShutdown();
@@ -45,7 +49,7 @@ describe('Basic gRPC Tests: ', () => {
         const data  = faker.lorem.words(2);
         const request = {data};
         client.Ping(request, callback);
-    })
+    });
 
     it('Can Add', function (done) {
         function callback(error, result) {
@@ -60,7 +64,7 @@ describe('Basic gRPC Tests: ', () => {
 
         const request = {numbers: [2, 3, 4, 5]};
         client.Add(request, callback);
-    })
+    });
 
     it('Can Subtract ', function (done) {
         function callback(error, result) {
@@ -75,7 +79,7 @@ describe('Basic gRPC Tests: ', () => {
 
         const request = {numbers: [102, 3, 4101, 19]};
         client.Subtract(request, callback);
-    })
+    });
 
     it('Can Multiple ', function (done) {
         function callback(error, result) {
@@ -90,7 +94,7 @@ describe('Basic gRPC Tests: ', () => {
 
         const request = {numbers: [102, 3, 4101, 19]};
         client.Multiply(request, callback);
-    })
+    });
 
     it('Can Divide', function (done) {
         function callback(error, result) {
@@ -99,12 +103,52 @@ describe('Basic gRPC Tests: ', () => {
                 done(error);
             }
             expect(result).to.be.an('object');
-            expect(result.result).to.equal(.000004773839360305525);
+            expect(result.result).to.equal(.002983649600190954);
             done()
         }
 
         const request = {numbers: [25, 7, 19, 63]};
         client.Divide(request, callback);
-    })
+    });
+
+    it('Can Ping', function (done) {
+        function callback(error, result) {
+            if (error) {
+                console.log(error);
+                done(error);
+            }
+            expect(result).to.be.an('object');
+            expect(result.result).to.equal(data);
+            done()
+        }
+
+        const data = faker.lorem.words(3);
+        client.Ping({data}, callback);
+    });
+
+    it('Can Repeat', function (done) {
+        const value = faker.lorem.words(3);
+        const limit = randomIntFromInterval(1,100);
+        let cnt = 0;
+        const call = client.Repeat({value,limit});
+        call.on('data', function (response) {
+            expect(response).to.be.an('object');
+            expect(response.value).to.equal(value);
+            expect(response.counter).to.equal(cnt);
+            cnt++;
+        });
+        call.on('end', function () {
+            expect(cnt).to.equal(limit);
+            done();
+        });
+        call.on('error', function (e) {
+            done(e);
+        });
+        call.on('status', function (status) {
+            console.log(`${cnt} messages returned`);
+        });
+
+    });
+
 
 });
